0e2989f513560b15b179ee3ee1cb07b922da7746
yazid1919 <121848394+yazid1919@users.noreply.github.com>
2023-08-30 16:57:45 +0700

    Update config.py
    Signed-off-by: yazid1919 <121848394+yazid1919@users.noreply.github.com>

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ except ValueError:
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nUntuk mendapatkan file/video dari bot ini anda harus join chanel dan group yang ada pada tombol di bawah, Lalu klik coba lagi. SUPPORT BRAND TELEGRAM => https://t.me/COLLECTOR_OFFICIAL</b>",
+    "<b>Hello {first}\n\nuntuk mendapatkan file atau video dari bot ini anda harus join chanel dan group yang ada pada tombol di bawah, Lalu klik coba lagi.SUPPORT BRAND TELEGRAM:https://t.me/COLLECTOR_OFFICIAL</b>",
 )
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
