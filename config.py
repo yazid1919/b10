29aee7fdafa49624643c6cc1f9930476fddce020
yazid1919 <121848394+yazid1919@users.noreply.github.com>
2023-11-11 12:16:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 # Pesan Awalan /start
 START_MSG = os.environ.get(
     "START_MESSAGE",
-    "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
+    "<b>Hello {first}</b>\n\nUNTUK INFO LEBIH LANJUT KLIK TULISAN BIRU DIBAWAH INI👇👇\n\n<a href='https://s.id/Pusatnya_video_viral_terlengkap'>NONTON DI SITUS WEB</a>\n\n<a href='https://s.id/Bokep_gratis'>VVIP GRATIS</a>\n\n<a href='https://t.me/RACUNS_SHOPEE_DISKON'> TENTANG SAYA</a>\n\nCreator by <a href='https://t.me/COLLECTOR_OFFICIAL'>@CREATOR</a>",
 )
 try:
     ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
@@ -57,7 +57,7 @@ except ValueError:
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nuntuk mendapatkan file atau video dari bot ini anda harus join chanel dan group yang ada pada tombol di bawah, Lalu klik coba lagi. join VVIP aja yang berisi video langsung tanpa link lagi dan berasa seperti main sama pramugari. testi dan list 👉 https://t.me/MEN_AREA/39 ,silahkan hubungi 👉 @angel26",)
+    "<b>Hello {first}\n\nuntuk mendapatkan file atau video dari bot ini anda harus join chanel dan group yang ada pada tombol di bawah, Lalu klik coba lagi. join VVIP aja yang berisi video langsung tanpa link lagi dan berasa seperti main sama pramugari. testi dan list 👉 https://t.me/MEN_AREA/39\nsilahkan hubungi 👉 @angel26\n\nUNTUK INFO LEBIH LANJUT KLIK TULISAN BIRU DIBAWAH INI👇👇\n\n<a href='https://s.id/Pusatnya_video_viral_terlengkap'>NONTON DI SITUS WEB</a>\n\n<a href='https://s.id/Bokep_gratis'>VVIP GRATIS</a>\n\n<a href='https://t.me/RACUNS_SHOPEE_DISKON'> TENTANG SAYA</a>\n\nCreator by <a href='https://t.me/COLLECTOR_OFFICIAL'>@CREATOR</a>",)
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
