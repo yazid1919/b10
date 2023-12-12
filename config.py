c298a87fb46ef6be860a7c93ad0cf3c37114f80a
yazid1919 <121848394+yazid1919@users.noreply.github.com>
2023-12-12 22:53:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ except ValueError:
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nuntuk mendapatkan file atau video dari bot ini anda harus join chanel dan group yang ada pada tombol di bawah, Lalu klik coba lagi. join VVIP aja yang berisi video langsung tanpa link lagi. testi dan list 👉 https://t.me/MEN_AREA/39\nsilahkan hubungi 👉 @angel26\n\nClick link dibawah ini👇👇\n\n=> https://s.id/koleksi-video-vcs\n\nNONTON DI SITUS WEB Click link dibawah ini 👇👇\n=> https://bokepbaratviral.blogspot.com\n\n<a href='https://t.me/RACUNS_SHOPEE_DISKON'> TENTANG SAYA</a>\n\nCreator by <a href='https://t.me/COLLECTOR_OFFICIAL'>@CREATOR</a>",)
+    "<b>Hello {first}\n\nuntuk mendapatkan file atau video dari bot ini anda harus join chanel dan group yang ada pada tombol di bawah, Lalu klik coba lagi. join VVIP aja yang berisi video langsung tanpa link lagi. testi dan list 👉 https://t.me/MEN_AREA/39\nsilahkan hubungi 👉 @angel26\n\nClick link dibawah ini👇👇\n=> https://s.id/Donasi-para-member\n\nNONTON DI SITUS WEB Click link dibawah ini 👇👇\n=> https://bokepbaratviral.blogspot.com\n\n<a href='https://t.me/RACUNS_SHOPEE_DISKON'> TENTANG SAYA</a>\n\nCreator by <a href='https://t.me/COLLECTOR_OFFICIAL'>@CREATOR</a>",)
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
