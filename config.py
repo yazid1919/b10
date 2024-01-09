b87b806c2bf56804c6a573f3f3901551af990018
yazid1919 <121848394+yazid1919@users.noreply.github.com>
2024-01-09 21:15:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 # Pesan Awalan /start
 START_MSG = os.environ.get(
     "START_MESSAGE",
-    "<b>Hello {first}</b>\n\nUNTUK INFO LEBIH LANJUT KLIK TULISAN BIRU DIBAWAH INI👇👇\n\n<a href='https://s.id/Pusatnya_video_viral_terlengkap'>NONTON DI SITUS WEB</a>\n\n<a href='https://s.id/Bokep_gratis'>VVIP GRATIS</a>\n\n<a href='https://t.me/RACUNS_SHOPEE_DISKON'> TENTANG SAYA</a>\n\nCreator by <a href='https://t.me/COLLECTOR_OFFICIAL'>@CREATOR</a>",
+    "<b>Hello {first}\n\n👇[𝗦𝗨𝗣𝗣𝗢𝗥𝗧 𝗦𝗜𝗧𝗨𝗦 𝗖𝗢𝗟𝗟𝗘𝗖𝗧𝗢𝗥]👇
+\n https://telegra.ph/SITUS-COLLECTOR-01-06 \n\n👇KOLEKSI VIDEO MEN AREA TERLENGKAP👇\n\nCapek mau nonton harus klik banyak link/bot?? Nonton disitus yang telah kami sediakan atau JOIN VVIP terlengkap kami\n\nNonton ribuan video langsung tanpa link lagi JOIN VVIP=> https://t.me/MEN_AREA/39 \nAtau bisa langsung hubungi=> @angel26\n\nKALIAN BISA NONTON GRATIS DISITUS YANG KAMI SEDIAKAN👇\n https://bit.ly/Nonton-video-viral-indo \n\nClick in here👇👇\n=>https://s.id/kumpulan-video-di-gubuk\n\nDeveloped by:\n<a href='https://t.me/+vwLf6fQUyqgyNmRl'>TENTANG SAYA</a>\nCreator by:\n<a href='https://t.me/+itziTCyvxX1mNzll'>@Creator</a>",
 )
 try:
     ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
@@ -57,7 +58,8 @@ except ValueError:
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nuntuk mendapatkan file atau video dari bot ini anda harus join chanel dan group yang ada pada tombol di bawah, Lalu klik coba lagi. join VVIP aja yang berisi video langsung tanpa link lagi. testi dan list 👉 https://t.me/MEN_AREA/39\nsilahkan hubungi 👉 @angel26\n\nClick link dibawah ini👇👇\n=> https://s.id/video-intip-kamar-mandi\n\nNONTON DI SITUS WEB Click link dibawah ini 👇👇\n=> https://bokepbaratviral.blogspot.com\n\n<a href='https://t.me/RACUNS_SHOPEE_DISKON'> TENTANG SAYA</a>\n\nCreator by <a href='https://t.me/COLLECTOR_OFFICIAL'>@CREATOR</a>",)
+    "<b>Hello {first}\n\n👇[𝗦𝗨𝗣𝗣𝗢𝗥𝗧 𝗦𝗜𝗧𝗨𝗦 𝗖𝗢𝗟𝗟𝗘𝗖𝗧𝗢𝗥]👇
+\n https://telegra.ph/SITUS-COLLECTOR-01-06 \n\n👇KOLEKSI VIDEO MEN AREA TERLENGKAP👇\n\nCapek mau nonton harus klik banyak link/bot?? Nonton disitus yang telah kami sediakan atau JOIN VVIP terlengkap kami\n\nNonton ribuan video langsung tanpa link lagi JOIN VVIP=> https://t.me/MEN_AREA/39 \nAtau bisa langsung hubungi=> @angel26\n\nKALIAN BISA NONTON GRATIS DISITUS YANG KAMI SEDIAKAN👇\n https://bit.ly/Nonton-video-viral-indo \n\nClick in here👇👇\n=>https://s.id/kumpulan-video-di-gubuk\n\nDeveloped by:\n<a href='https://t.me/+vwLf6fQUyqgyNmRl'>TENTANG SAYA</a>\nCreator by:\n<a href='https://t.me/+itziTCyvxX1mNzll'>@Creator</a>",)
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
